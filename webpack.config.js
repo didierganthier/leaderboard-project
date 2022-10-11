6907cf6e3719f6625d2f551a82c59e6d5f18ade0
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-11 09:20:58 -0400

    Set-up webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin(

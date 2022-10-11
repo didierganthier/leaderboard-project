003a96265a39e30b2ee441bc298fd052b1eeda76
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-11 09:13:07 -0400

    Set-up webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        title: 'Leaderboard',
+        template: './src/index.html',
+      },
+    ),
+  ],
+};
